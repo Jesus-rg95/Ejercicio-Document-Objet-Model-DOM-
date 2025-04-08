e3be97ea6ed068f84c8c110203cdd8842374694e
jesusgr <jesusgr9511@gmail.com>
2025-04-08 12:19:07 -0600

    Se agregan cambios

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-let btnMostrar = document.getElementById("btnMostar");
 let encabezado1 = document.getElementById("encabezado1");
-let encabezado2= document.getElementById("encabezado2");
-let listas = document.getElementsByClassName("ul");
-
+let encabezado2 = document.getElementById("encabezado2");
+let lista = document.getElementsByTagName("ul");
 let elementos = document.getElementsByClassName("list-group-item");
+let otroElemento = document.querySelector("ul>li");
+let otrosElementos = document.querySelectorAll("ul>li");
+let btnMostrar = document.getElementById("btnMostrar");
 
-let otroElemento = document.querySelector("ul>li");//primero que encuentra
-console.log("otroElemento:", otroElemento);
+let contador=0;
 
-let otrosElementos =document.querySelectorAll("ul>li");//todos
-
-
-console.log(listas.length);//2
+console.log(lista.length);//2
 //console.log(listas[0]);
 
-console.log(listas.item(1));
+console.log(lista.item(1));
 
 console.log(elementos.item(2));
 
 console.log(elementos.length);//10
 
-let contador=0;
+
 function modifica(){
     encabezado2.innerHTML += "<em>Ejercicio</em>DOM";
     encabezado1.innerText = ++contador;
@@ -30,6 +27,21 @@ function modifica(){
 // console.log(encabezado1.innerText);
 
 btnMostrar.addEventListener("click", function(event){
-    event.preventDefault(); /// no hagas lo que haces por defecto
+    event.preventDefault(); //no hagas lo que haces por defecto
+    console.log("Botón btnModificar presionado");
+
+    let element = document.createElement("li");
+    element.innerText= "Another item";// <li> Anoter item </li>
+    element.classList.add("list-group-item");
+    
+    let element2 = element.cloneNode(true);
+
+   // listas.item(0).before(element);
+   // listas.irem(0).prepend(element2);
+
+    listas.item(0).append(element);
+    listas.irem(0).after(element2);
+
+
 
 });
